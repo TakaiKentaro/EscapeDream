ae9bb07d59c39b11e8b3992edf8c05053005a424
TakaiKentarou <mmpm664@yahoo.co.jp>
2021-11-05 15:47:34 +0900

    プレイヤー完成

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float m_moveSpeed = 1f;
+    Rigidbody m_rb = default;
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = Camera.main.transform.TransformDirection(dir);
+        dir.y = 0;
+
+        if (dir != Vector3.zero)
+        {
+            this.transform.forward = dir;
+        }
+
+        m_rb.velocity = dir.normalized * m_moveSpeed + m_rb.velocity.y * Vector3.up;
+    }
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float m_moveSpeed = 1f;
+    Rigidbody m_rb = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = Camera.main.transform.TransformDirection(dir);
+        dir.y = 0;
+
+        if (dir != Vector3.zero)
+        {
+            this.transform.forward = dir;
+        }
+
+        m_rb.velocity = dir.normalized * m_moveSpeed + m_rb.velocity.y * Vector3.up;
+    }
+}
