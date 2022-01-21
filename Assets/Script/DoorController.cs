e7d4d359f2e85b39b3bfae31a9d02e890444797d
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-01-21 20:31:04 +0900

    扉途中

---
+++ Assets/Script/DoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Animator _anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            _anim.Play("OpenDoor");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _anim.Play("OpenDoor",-1);
+        }
+    }
+}
