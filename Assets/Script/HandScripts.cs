71d0d3f99b1605a2eb537c267c32bcc93b75624f
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-02-08 18:20:49 +0900

    LockDoorPanel完成‼

--- Assets/Script/HandScripts.cs
+++ Assets/Script/HandScripts.cs
@@ -12,6 +12,7 @@ public class HandScripts : MonoBehaviour
     [SerializeField] GameObject _sceneManager;
 
     [Header("アイテム管理")]
+    [SerializeField] GameObject _enemyObj;
     [SerializeField] GameObject _flashLight;
     [SerializeField] GameObject _itemManager;
     [SerializeField] GameObject _panel;
@@ -27,6 +28,7 @@ public class HandScripts : MonoBehaviour
     bool _isItem;
     bool _isChest;
     bool _isLockPanel;
+    bool _isPickStone;
     public bool _moveStop;
     public bool _isPicture;
     Collider _itemCollider;
@@ -45,8 +47,10 @@ public class HandScripts : MonoBehaviour
         KeyInput();
         DoorInOut();
         Chest();
+        LockPanel();
         LookImage(_picCollider);
         TakeLight(_lightCollider);
+        StoneInput(_pickStoneCollider);
     }
 
     void KeyInput()
@@ -60,6 +64,32 @@ public class HandScripts : MonoBehaviour
                 Destroy(_itemCollider.gameObject);
                 Invoke(nameof(ResetText), 2);
             }
+        }
+    }
+    void StoneInput(Collider other)
+    {
+        if(_isPickStone && Input.GetButtonDown("Item"))
+        {
+            switch (other.gameObject.name)
+            {
+                case "MaruStone":
+                    _displayText.text = "丸い石を拾った";
+                    _lockPanel.GetComponent<LockPanelScript>()._maruCheck = true;
+                    Destroy(_pickStoneCollider.gameObject);
+                    break;
+                case "SankakuStone":
+                    _displayText.text = "三角形の石を拾った";
+                    _enemyObj.SetActive(true);
+                    _lockPanel.GetComponent<LockPanelScript>()._sankakuCheck = true;
+                    Destroy(_pickStoneCollider.gameObject);
+                    break;
+                case "SikakuStone":
+                    _displayText.text = "四角い石を拾った";
+                    _lockPanel.GetComponent<LockPanelScript>()._sikakuCheck = true;
+                    Destroy(_pickStoneCollider.gameObject);
+                    break;
+            }
+            Invoke(nameof(ResetText), 2);
         }
     }
     void DoorInOut()
@@ -119,8 +149,9 @@ public class HandScripts : MonoBehaviour
     {
         if(_takeLight && Input.GetButtonDown("Item"))
         {
-            Destroy(other.gameObject);
-            _flashLight.SetActive(true);
+            other.gameObject.SetActive(false);
+            Destroy(_flashLight.gameObject);
+            ResetText();
         }
     }
 
@@ -128,6 +159,7 @@ public class HandScripts : MonoBehaviour
     {
         if(_isLockPanel && Input.GetButtonDown("Item"))
         {
+            _cursorManager.GetComponent<CursorManeger>().m_cursor = true;
             _lockPanel.SetActive(true);
             _camera.SetActive(false);
             _moveStop = true;
@@ -166,9 +198,12 @@ public class HandScripts : MonoBehaviour
                 _lightCollider = other;
                 break;
             case "Stone":
+                _displayText.text = "”Eキー”拾う";
+                _isPickStone = true;
+                _pickStoneCollider = other;
                 break;
             case "LockPanel":
-                _displayText.text = "”Eキー”見る";
+                _displayText.text = "”Eキー”調べる";
                 _isLockPanel = true;
                 break;
         }
@@ -213,9 +248,10 @@ public class HandScripts : MonoBehaviour
         _isChest = false;
         _panel.SetActive(false);
         _isPicture = false;
-        _cursorManager.GetComponent<CursorManeger>().m_cursor = false;
         _takeLight = false;
         _isLockPanel = false;
+        _moveStop = false;
+        _isPickStone = false;
         //_camera.SetActive(true);
     }
 }
--- Assets/Script/LockPanelScript.cs
+++ Assets/Script/LockPanelScript.cs
@@ -18,44 +18,59 @@ public class LockPanelScript : MonoBehaviour
     [SerializeField] Text _checkText;
 
     [Header("扉")]
-    [SerializeField] GameObject _lockDoor;
+    [SerializeField] GameObject _moveLockDoor;
+    [SerializeField] GameObject _lockPanel;
 
+    Animator _anim;
+    private void Start()
+    {
+        _anim = GameObject.Find(_moveLockDoor.name).GetComponent<Animator>();
+    }
     public void OnClickmaru()
     {
         if(_maruCheck)
         {
             _checkText.text = "〇の石がはまった";
+            _buttons[0].GetComponent<Image>().sprite = _sprits[0]; 
             LockClearCheck();
+            Invoke(nameof(ResetText), 2);
         }
         else
         {
             _checkText.text = "はまる石がない";
+            Invoke(nameof(ResetText), 2);
         }
     }
 
     public void OnClicksikaku()
     {
-        if (_maruCheck)
+        if (_sikakuCheck)
         {
             _checkText.text = "▢の石がはまった";
+            _buttons[2].GetComponent<Image>().sprite = _sprits[2];
             LockClearCheck();
+            Invoke(nameof(ResetText), 2);
         }
         else
         {
             _checkText.text = "はまる石がない";
+            Invoke(nameof(ResetText), 2);
         }
     }
 
     public void OnClicksankaku()
     {
-        if (_maruCheck)
+        if (_sankakuCheck)
         {
             _checkText.text = "△の石がはまった";
+            _buttons[1].GetComponent<Image>().sprite = _sprits[1];
             LockClearCheck();
+            Invoke(nameof(ResetText), 2);
         }
         else
         {
             _checkText.text = "はまる石がない";
+            Invoke(nameof(ResetText), 2);
         }
     }
 
@@ -65,9 +80,16 @@ public class LockPanelScript : MonoBehaviour
     void LockClearCheck()
     {
         //ここに扉が開く処理
-        if(_maruCheck && _sankakuCheck && _sikakuCheck)
+        if (_maruCheck && _sankakuCheck && _sikakuCheck)
         {
-
+            _checkText.text = "扉が開いた";
+            //_lockPanel.SetActive(false);
+            _anim.SetBool("OpenDoor", true);
         }
+    }
+
+    void ResetText()
+    {
+        _checkText.text = "";
     }
 }
