0cd1cc9e9c4cc57181a9c4f0635d57a74e601e41
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-02-11 10:39:30 +0900

    アセット追加(マテリアル・家)

--- Assets/Script/RadioSounds.cs
+++ Assets/Script/RadioSounds.cs
@@ -2,28 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RadioSounds : MonoBehaviour
+public class AudioSounds : MonoBehaviour
 {
-    AudioSource _audioSource;
 
-    public int _count = 0;
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        _audioSource = GetComponent<AudioSource>();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        
-        if (other.gameObject.tag == "FlashLight")
+        if(other.gameObject.tag == "Player")
         {
-            _count++;
-            Debug.Log(_count);
-            if (_count >= 3)
-            {
-                _audioSource.PlayOneShot(_audioSource.clip);
-            }
+            Debug.Log("あたった");
         }
     }
 }
